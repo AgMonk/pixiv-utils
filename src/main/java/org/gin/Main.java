c572acbca908a5a1de9c02ba45744f2abf919911
ginstone <hjg719@139.com>
2022-10-10 14:12:50 +0800

    Initial commit

---
+++ src/main/java/org/gin/Main.java
@@ -0,0 +1,10 @@
+package org.gin;/**
+ * ${TODO}
+ * @since : ${DATE} ${HOUR}:${MINUTE}
+ * @author : ginstone
+ * @version : v1.0.0
+ **/public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
