6145423e4fad9a784aee20340f51a8097cf1ee54
ginstone <hjg719@139.com>
2022-10-11 17:59:52 +0800

    url

--- src/main/java/org/gin/request/PixivUrl.java
+++ src/main/java/org/gin/request/PixivUrl.java
@@ -4,14 +4,39 @@ import okhttp3.HttpUrl;
 
 import java.util.Objects;
 
+import static org.gin.request.PixivCommon.*;
+
 /**
  * @author : ginstone
  * @version : v1.0.0
  * @since : 2022/10/11 17:27
  **/
 public class PixivUrl {
-    public static final String URL_ILLUST_DETAIL = PixivCommon.DOMAIN_AJAX + "illust/%d";
-    public static final String URL_FOLLOW_LATEST = PixivCommon.DOMAIN_AJAX + "follow_latest/illust";
+    /**
+     * 作品详情接口 cookie可选
+     */
+    public static final String URL_ILLUST_DETAIL = DOMAIN_AJAX + "illust/%d";
+    /**
+     * 查询关注作者最新作品的URL 需要cookie
+     */
+    public static final String URL_FOLLOW_LATEST = DOMAIN_AJAX + "follow_latest/illust";
+    /**
+     * 获取收藏作品接口 需要cookie
+     */
+    public static final String URL_BOOKMARKS_GET = DOMAIN_AJAX + "user/%d/illusts/bookmarks";
+    /**
+     * 添加收藏 需要cookie、token
+     */
+    public static final String ADD_BOOKMARKS = DOMAIN_AJAX + "illusts/bookmarks/add";
+    /**
+     * 删除收藏
+     */
+    public static final String DEL_BOOKMARKS = DOMAIN + "rpc/index.php";
+    /**
+     * 搜索作品
+     */
+    public static final String SEARCH_ARTWORKS = DOMAIN_AJAX + "search/artworks/%s";
+
 
     /**
      * 生成查询关注作者最新作品的URL
