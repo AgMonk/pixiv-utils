5104eadcbf583a7fe3f3253966aef4ff2e9ec400
ginstone <hjg719@139.com>
2024-01-13 11:03:20 +0800

    修

--- src/main/java/com/gin/pixiv/params/LatestParam.java
+++ src/main/java/com/gin/pixiv/params/LatestParam.java
@@ -1,20 +1,23 @@
 package com.gin.pixiv.params;
 
 import com.gin.pixiv.enums.PixivMode;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * 请求关注作者的最新作品的参数
+ *
  * @author : ginstone
  * @version : v1.0.0
  * @since : 2023/3/29 12:17
  */
 @Getter
-@RequiredArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 public class LatestParam {
-    final int page;
+    int page = 1;
     @NotNull
-    final PixivMode mode;
+    PixivMode mode = PixivMode.all;
 }   
